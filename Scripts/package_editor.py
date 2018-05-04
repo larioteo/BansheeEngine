37d5b382522c6c1824c5d06c0730e50072322cd1
BearishSun <bearishsun@gmail.com>
2018-05-04 17:48:26 +0200

    Build: Updated editor package script to match with the new repository structure

--- Scripts/package_editor.py
+++ Scripts/package_editor.py
@@ -12,46 +12,59 @@ import sys
 import shutil
 
 configuration = 'OptimizedDebug' #sys.argv[1]
-dataEntriesToIgnore = ['Examples', 'Raw', 'Settings.asset']
-binEntriesToIgnore = ['Game.exe', 'Game.pdb', 'ExampleProject.exe', 'ExampleProject.pdb',
-                      'BansheeD3D9RenderAPI.dll', 'BansheeD3D9RenderAPI.pdb']
+dataEntriesToIgnore = ['Raw', 'Settings.asset']
+binEntriesToIgnore = ['Game.exe', 'Game.pdb']
 
 dataFolder = 'Data'
+bsfDataFolder = 'Source\\bsf\\Data'
 assembliesFolder = 'Assemblies'
 monoFolder = 'Mono'
 libFolder = 'x64\\' + configuration + '\\'
 
 inputDataFolder = '..\\' + dataFolder
+inputBsfDataFolder = '..\\' + bsfDataFolder
 inputBinFolder = '..\\bin\\'
 inputAssembliesFolder = inputBinFolder + assembliesFolder
 inputMonoFolder = inputBinFolder + monoFolder
 inputLibFolder = inputBinFolder + libFolder
 
-outputBaseFolder = '..\\Builds\Banshee Editor\\'
+outputBaseFolder = '..\\Builds\Banshee3D\\'
+outputEditorDataFolder = outputBaseFolder + 'EditorData'
 outputDataFolder = outputBaseFolder + dataFolder
 outputBinFolder = outputBaseFolder + '\\bin\\'
 outputAssembliesFolder = outputBinFolder + assembliesFolder
 outputMonoFolder = outputBinFolder + monoFolder
 outputLibFolder = outputBaseFolder
 
-def ignore_data(path, entries):
-    if path != inputDataFolder:
-        return []
-
-    return list(set(dataEntriesToIgnore) & set(entries))    
-
+def copytree(src, dst):
+    if not os.path.exists(dst):
+        os.makedirs(dst)
+    
+    for item in os.listdir(src):
+        if item in dataEntriesToIgnore:
+            print("Ignoring " + item)
+            continue
+        
+        s = os.path.join(src, item)
+        d = os.path.join(dst, item)
+        print(s)
+        if os.path.isdir(s):
+            copytree(s, d)
+        else:
+            shutil.copy2(s, d)	
+	
 def package_editor():
     if os.path.exists(outputBaseFolder):
         shutil.rmtree(outputBaseFolder)
 
-    os.makedirs(outputBaseFolder)
-    shutil.copytree(inputDataFolder, outputDataFolder, False, ignore_data)
-    shutil.copytree(inputAssembliesFolder, outputAssembliesFolder)
-    shutil.copytree(inputMonoFolder, outputMonoFolder)
+    copytree(inputDataFolder, outputEditorDataFolder)
+    copytree(inputBsfDataFolder, outputDataFolder)
+    copytree(inputAssembliesFolder, outputAssembliesFolder)
+    copytree(inputMonoFolder, outputMonoFolder)
 
     for root, dirs, files in os.walk(inputLibFolder):
         for file in files:
-            if(file.lower().endswith(('.dll', '.exe', '.pdb', '.so'))):
+            if(file.lower().endswith(('.dll', '.exe', '.pdb', '.so', '.dylib'))):
                 if(not file in binEntriesToIgnore):
                     filePath = os.path.join(root, file)
                     shutil.copy(filePath, outputLibFolder)
